0c970315ef394e7c72b3c8745430a988a2ce2e55
Guillaume Lincot <guillaumelincot22@gmail.com>
2024-01-24 13:34:37 +0100

    test event listener

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ var o = context.createOscillator()
 o.type = "sine"
 o.connect(context.destination)
 o.start()
+
+/*document.getElementById('play-sound').addEventListener('click', function() {
+    var audioContext = new AudioContext();
+    var oscillator = audioContext.createOscillator();
+    oscillator.frequency.setValueAtTime(440, audioContext.currentTime); // Fréquence en Hz (La à 440Hz)
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
+    oscillator.stop(audioContext.currentTime + 1); // Joue le son pendant 1 seconde
+});*/
