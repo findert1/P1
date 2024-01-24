4f439081da5c248a7c33d095e5a3561c07a9dd32
Guillaume Lincot <guillaumelincot22@gmail.com>
2024-01-24 13:38:42 +0100

    not remove

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ o.type = "sine"
 o.connect(context.destination)
 o.start()
 
-/*document.getElementById('play-sound').addEventListener('click', function() {
+document.getElementById('play-sound').addEventListener('click', function() {
     var audioContext = new AudioContext();
     var oscillator = audioContext.createOscillator();
     oscillator.frequency.setValueAtTime(440, audioContext.currentTime); // Fréquence en Hz (La à 440Hz)
     oscillator.connect(audioContext.destination);
     oscillator.start();
     oscillator.stop(audioContext.currentTime + 1); // Joue le son pendant 1 seconde
-});*/
+});
