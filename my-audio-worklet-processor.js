da18a461466c242b5b9e26866a35a18e6267c414
Thomas Boulineau <th.boulineau@gmail.com>
2024-03-11 23:34:20 +0100

    fonctionne, manque à établir un seuil pour 0 ou 1

--- my-audio-worklet-processor.js
+++ my-audio-worklet-processor.js
@@ -1,78 +1,123 @@
 // Définir le traitement audio dans cette classe
-class MyAudioWorkletProcessor extends AudioWorkletProcessor {
-  constructor() {
-    super();
-    this.freq_lsb = freq_lsb;
-    this.step = step;
-    this.duration = duration;
-    this.sample_rate = sample_rate;
-    this.window; // pour ne pas calculer la fenêtre à chaque appel
 
+//import * from AudioWorkletProcessor;
+
+class MyAudioWorkletProcessor extends AudioWorkletProcessor {
+  constructor(options) {
+    super(options);
+    console.log("Construction de l'instance du processing du son...");
+    // constantes pour les calculs
+    this.freq_lsb = 3000;
+    this.step = 333;
+    this.duration = 0.5;
+
+    this.setup = false;
+    
+
+    this.window; // pour ne pas calculer la fenêtre à chaque appel
     this.result = new Array(4).fill(0);
     this.target_fequency = new Array(4);
-    for(let i=0; i<4; i++){
-      this.target_fequency[i] = this.freq_lsb + i * this.step;
-    }
-
     this.target_index = new Array(4);
 
+    this.port.onmessage = this.handleMessage.bind(this);
+
+    console.log("Instance processing déclarée");
+    console.log("Sample rate : " + sampleRate);
   }
 
-  getHammingWindow(N) {
-    let window = new Array(N);
+  handleMessage(event){
+    const message = event.data;
+    this.port.postMessage({ result: message.result });
+  }
+
+  setHammingWindow(N) {
+    console.log("Calcul de la fenêtre de Hamming...");
+    this.window = new Array(N);
     for (let i = 0; i < N; i++) {
-        window[i] = 0.54 - 0.46 * Math.cos(2 * Math.PI * i / (N - 1));
+        this.window[i] = 0.54 - 0.46 * Math.cos(2 * Math.PI * i / (N - 1));
     }
-    return window;
+    console.log("Fenêtre de Hamming : " + this.window);
+    console.log("Taille : " + this.window.length);
+  }
+
+  setTargetFrequencies(N){
+    console.log("Remplissage du tableau des fréquences cible...");
+    for(let i=0; i<4; i++){
+      this.target_fequency[i] = this.freq_lsb + this.step * i;
+    }
+    console.log("Fréquences cible : " + this.target_fequency);
+
+    console.log("Remplissage du tableau des index cible...");
+    for(let i=0; i<4; i++){
+      this.target_index[i] = Math.round(this.target_fequency[i] / sampleRate * N);
+    }
+    console.log("Indices cible : " + this.target_index);
   }
 
   process(inputs, outputs, parameters) {
-    let inputBuffer = new Array(inputs[0].length).fill(0);
-    if(this.window == null){
-      this.window = this.getHammingWindow(inputBuffer.length);
-
-      for(let i=0; i<4; i++){
-        this.target_index = Math.round(target_fequency[i] / sampleRate * inputBuffer.length);
-      }
+    //console.log(inputs[0][0]);
+    let inputBuffer = new Array(inputs[0][0].length).fill(0);
+    if(!this.setup){
+      console.log("Initialisation process...");
+      this.setHammingWindow(inputBuffer.length);
+      this.setTargetFrequencies(inputBuffer.length);
+      this.setup = true;
+      console.log("Fin du process d'initialisation.");
     }
+
     // si l'entrée est constituée de 2 canaux méthode RMS
-    if(inputs.length >= 2){
+    //console.log("Nombre d'entrées : " + inputs.length); 
+    // 1 entrée
+    //console.log("Nombre de canaux d'entrée : " + inputs[0].length);
+    
+    // 2 canaux d'entrée
+    if(inputs[0].length > 1){
       for(let i=0; i<inputBuffer.length; i++){
         let totalSq = 0;
-        for(let j=0; j<inputs.length; j++){
-          totalSq += inputs[j][i] ** 2;
+        for(let j=0; j<inputs[0].length; j++){
+          totalSq += inputs[0][j][i] ** 2;
         }
         inputBuffer[i] = Math.sqrt(totalSq);
       }
     }
+
+    //console.log("Input buffer : " + inputBuffer);
 
     let dft = new Array(8).fill(0); // 8 car 4 bits * 2 pour réel et imaginaire
     let dftBuffer = new Array(inputBuffer.length).fill(0);
     let amplitude = new Array(4);
 
     // Application de la fenêtre de Hamming
-    for (let i = 0; i < dftBuffer; i++) {
+    for (let i = 0; i < dftBuffer.length; i++) {
       dftBuffer[i] = inputBuffer[i] * this.window[i];
-    }    
-
+    }
+    //console.log("dftBuffer : " + dftBuffer);
 
     // Calcul de la dft
     for(let i=0; i<dft.length; i++){
       for (let j = 0; j < dftBuffer.length; j++) { // pour chaque du buffer
-        dft[i] += dftBuffer[j] * Math.cos(2 * Math.PI * this.target_index[i] * j / dftBuffer.length);
+        dft[i] += dftBuffer[j] * Math.cos(2 * Math.PI * this.target_index[Math.floor(i/2)] * j / dftBuffer.length);
       }
     }
+    //console.log(dft);
 
     // Extraction de l'amplitude de la fréquence cible
     for(let i=0; i<amplitude.length; i++){
-      amplitude = Math.sqrt(dft[i] ** 2 + dft[i + 1] ** 2);
+      let val = Math.sqrt(dft[i] ** 2 + dft[i + 1] ** 2);
+      amplitude[i] = val * 100;
     }
     
-    console.log(amplitude);
+    //console.log(amplitude);
 
-    return true; // Indique que le traitement a été effectué avec succès
+    this.port.postMessage({ result: amplitude });
+    //console.log(amplitude);
+
+    return true;
   }
 }
   
 // Enregistrer le module de travail audio
-registerProcessor('my-audio-worklet-processor', MyAudioWorkletProcessor);
+registerProcessor("my-audio-worklet-processor", MyAudioWorkletProcessor);
+//export default MyAudioWorkletProcessor;
+
+//https://github.com/WebAudio/web-audio-api/issues/1942
--- script4.js
+++ script4.js
@@ -2,30 +2,42 @@ const FREQ_LSB = 3000;
 const STEP = 333;
 const DURATION = 0.5;
 
+//import * as mod from "./my-audio-worklet-processor.js";
+
 async function setupAudioProcessing() {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const audioContext = new AudioContext();//(window.AudioContext || window.webkitAudioContext)();
     const input = audioContext.createMediaStreamSource(stream);
-  
+
     // Obtenez le nombre de canaux d'entrée
     const numberOfInputChannels = input.channelCount;
     console.log("Nombre de canaux d'entrée : " + numberOfInputChannels);
-  
+
     // Créer un AudioWorkletNode pour le traitement audio
     await audioContext.audioWorklet.addModule('my-audio-worklet-processor.js');
-    const audioWorkletNode = new AudioWorkletNode(audioContext, 'my-audio-worklet-processor', {
-        sample_rate: audioContext.sampleRate,
-        freq_lsb: FREQ_LSB,
-        step: STEP,
-        duration: DURATION
-    });
-  
+    const audioWorkletNode = new AudioWorkletNode(audioContext, 'my-audio-worklet-processor');
+
     // Connecter l'entrée à l'AudioWorkletNode
     input.connect(audioWorkletNode);
+    console.log("connecté");
 
-  }
+    audioWorkletNode.port.onmessage = function(event) {
+        const result = event.data.result;
+        //console.log(result);
+        document.getElementById("result1").textContent = result[0];
+        document.getElementById("result2").textContent = result[1];
+        document.getElementById("result3").textContent = result[2];
+        document.getElementById("result4").textContent = result[3];
+    }
+}
 
-// Appeler la fonction pour configurer le traitement audio
-setupAudioProcessing().catch(error => {
-    console.error('Erreur lors de la configuration du traitement audio:', error);
+let listeningActive = false;
+
+document.getElementById("start-button").addEventListener("click", function(){
+    // Appeler la fonction pour configurer le traitement audio
+    setupAudioProcessing().catch(error => {
+        console.error('Erreur lors de la configuration du traitement audio:', error);
+    });
 });
+
+
