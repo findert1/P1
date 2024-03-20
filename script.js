6867641eede23d9f46036cf61eb489d41ba15536
Thomas Boulineau <th.boulineau@gmail.com>
2024-03-20 14:45:27 +0100

    zeljfhkqsf

--- script.js
+++ script.js
@@ -3,77 +3,74 @@ let audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const textInput = document.getElementById("text-input");
 const sendButton = document.getElementById("send-sound");
 const TONE_LENGTH_MS = 250;
-const ADDITIONAL_DELAY_MS = 2000;  // Augmentez si nécessaire pour vous assurer que le son de départ est joué
+const ADDITIONAL_DELAY_MS = 0;  // Augmentez si nécessaire pour vous assurer que le son de départ est joué
 
 // Nouvelles fréquences
 const START_FREQ = 10024;
 const END_FREQ = 17000;
 const STEP_FREQ = 20;
 
+let serverTime;
+let ecartAbsoluLocal;
+syncClockWithNTP();
+
+//setInterval(synchroniserHeureAvecNTP, 1000);
+
 sendButton.addEventListener("click", function() {
     if(audioContext.state === "suspended") {
         audioContext.resume().then(() => {
             console.log("AudioContext resumed successfully");
-            startSending();
+            envoiTexte();
         });
     } else {
-        startSending();
+        envoiTexte();
     }
 });
 
-function startSending() {
-    const textToEncode = textInput.value;
-    // Introduisez un délai avant de commencer à envoyer
-    setTimeout(() => sendTextAsSound(textToEncode), ADDITIONAL_DELAY_MS);
+function envoiTexte() {
+  const maintenantLocal = Date.now();
+  const maintenantAbsolu = maintenantLocal + ecartAbsoluLocal;
+  const heureEnvoiAbsolu = Math.ceil(maintenantAbsolu / 1000) * 1000;
+  const delaiEnvoi = heureEnvoiAbsolu - maintenantLocal;
+  console.log("Signal envoyé dans " + delaiEnvoi.toString());
+  document.getElementById("displayDelai").innerText = "Signal envoyé dans " + delaiEnvoi.toString(); 
+  setTimeout(() => sendTextAsSound(textInput.value), delaiEnvoi);
 }
 
 function sendTextAsSound(text) {
     let tones = [START_FREQ]; // Commencez par ajouter le son de départ
-
     for (let i = 0; i < text.length; i++) {
         tones.push(charToFrequency(text.charAt(i)));
     }
-
     tones.push(END_FREQ); // Ajoutez le son de fin
 
-    playTonesSequentially(tones, 0);
-}
-
-function playTonesSequentially(tones, index) {
-    if (index < tones.length) {
-        playTone(tones[index], TONE_LENGTH_MS, () => playTonesSequentially(tones, index + 1));
-    }
-}
-
-function playTone(freq, duration, callback) {
-    // Vérifier l'état de l'`AudioContext` et le reprendre si nécessaire
     if (audioContext.state === 'suspended') {
-        audioContext.resume().then(() => {
-            console.log('AudioContext activated just before playing the tone.');
-            actuallyPlayTone(freq, duration, callback);
-        });
-    } else {
-        actuallyPlayTone(freq, duration, callback);
+      audioContext.resume().then(() => {
+          console.log('AudioContext activated just before playing the tone.');
+      });
+    } 
+
+    for(let i=0; i<tones.length; i++){
+      playFrequency(tones[i], audioContext.currentTime + (TONE_LENGTH_MS * i)/1000);
+      //console.log(i);
     }
+
 }
 
-function actuallyPlayTone(freq, duration, callback) {
-    let oscillator = audioContext.createOscillator();
+function playFrequency(freq, debutAudio) {
+    let oscillator = audioContext.createOscillator(); // un seul oscillateur pour chaque son
     let gainNode = audioContext.createGain();
 
     oscillator.type = 'sine';
-    oscillator.frequency.setValueAtTime(freq, audioContext.currentTime);
-    gainNode.gain.setValueAtTime(0.1, audioContext.currentTime);
+    oscillator.frequency.setValueAtTime(freq, debutAudio);
+    gainNode.gain.setValueAtTime(0.1, debutAudio);
 
     oscillator.connect(gainNode);
     gainNode.connect(audioContext.destination);
 
-    oscillator.start();
-    oscillator.stop(audioContext.currentTime + duration / 1000);
-
-    if (callback) {
-        setTimeout(callback, duration);
-    }
+    oscillator.start(debutAudio);
+    //console.log(new Date(Date.now()).getMilliseconds());
+    oscillator.stop(debutAudio + TONE_LENGTH_MS / 1000);
 }
 
 function charToFrequency(caractere){
@@ -101,91 +98,41 @@ function frequencyToChar(frequence){
   }
 }
 
+// variables
+const analyser = audioContext.createAnalyser();
+analyser.fftSize = 16384;
+let frequencyResolution = audioContext.sampleRate / analyser.fftSize;
+let isListening = false;
+let animationFrameId;
+var register = new Array(); // enregistrement des fréquences
+var result = new Array(); // mot de sortie
+var index = 0;
+var indexLettre = 0;
+const seuil = 100;
+const seuil_temps = 180;
+const marge = 3; // diffénrece de fréquences entre 2 lettres
 
+const bufferLength = analyser.frequencyBinCount;
+const frequencyData = new Uint8Array(bufferLength);
+
+// Get the reference to the HTML elements
+const maxFrequencyElement = document.getElementById('max-frequency');
+const maxFrequencyValueElement = document.getElementById('max-frequency-value');
+
+const seuilElement = document.getElementById('seuil');
+seuilElement.textContent = `Seuil : ${seuil}`;
+
+let chrono = performance.now();
+
+
+
+// validation de l'autorisation d'accès au micro par l'utilisateur
 
 document.addEventListener('DOMContentLoaded', () => {
-
-
-
-    const analyser = audioContext.createAnalyser();
-    analyser.fftSize = 16384;
-    let frequencyResolution = audioContext.sampleRate / analyser.fftSize;
-    let isListening = false;
-    let animationFrameId;
-    var register = new Array(); // enregistrement des fréquences
-    var result = new Array(); // mot de sortie
-    var index = 0;
-    var indexLettre = 0;
-    const seuil = 100;
-    const seuil_temps = 180;
-    const marge = 3; // diffénrece de fréquences entre 2 lettres
-
-    let chrono = performance.now();
-
     navigator.mediaDevices.getUserMedia({ audio: true })
       .then((stream) => {
         const source = audioContext.createMediaStreamSource(stream);
         source.connect(analyser);        
-
-        const bufferLength = analyser.frequencyBinCount;
-        const frequencyData = new Uint8Array(bufferLength);
-
-        // Get the reference to the HTML elements
-        const maxFrequencyElement = document.getElementById('max-frequency');
-        const maxFrequencyValueElement = document.getElementById('max-frequency-value');
-
-        const seuilElement = document.getElementById('seuil');
-        seuilElement.textContent = `Seuil : ${seuil}`;
-        //const freqMinElement = document.getElementById('freqMin');
-        //freqMinElement.textContent = `Indice de fréquence minimale : ${indexFreqMin}`;
-
-        function updateFrequencyData() {
-          if (!isListening) {
-            return; // Stop updating when not listening
-          }
-          analyser.getByteFrequencyData(frequencyData);
-
-          // mettre toutes les fréquences en-dessous de la fréquence min à 0
-          for(let i=0; i<-1; i++){
-            frequencyData[i] = 0;
-          }
-
-          // Find the index of the maximum value in the frequencyData array
-
-          const maxIndex = frequencyData.indexOf(Math.max(...frequencyData));
-          maxFrequencyElement.textContent = `Max Frequency: ${maxIndex * frequencyResolution}`;
-          maxFrequencyValueElement.textContent = `Max Frequency Value: ${frequencyData[maxIndex]}`;
-
-          if(frequencyData[maxIndex] >= seuil && maxIndex > START_FREQ/frequencyResolution){
-            console.log("Son au-dessus des seuils détecté"); 
-            register[index] = maxIndex; 
-            
-            // Détection du changement de fréquence
-            if(index > 1){
-              if(register[index] > register[index-1] + marge || register[index] < register[index-1] - marge){
-                console.log("Changement de fréquence détecté");
-                // Mesure du temps pendant lequel une fréquence a été émise
-                let temps = performance.now() - chrono;
-                if(temps > seuil_temps){
-                  var lettreTemp = frequencyToChar(register[index-2] * frequencyResolution);
-                  for(let i=0; i< Math.floor(temps/seuil_temps); i++){
-                    result[indexLettre] = lettreTemp;
-                    console.log(lettreTemp);
-                    indexLettre ++;
-                  }
-                  console.log("Temps de la lettre : " + temps);
-                }
-                
-                chrono = performance.now();
-              }
-            }else{
-              chrono = performance.now();
-            }
-            index++;
-          }
-          requestAnimationFrame(updateFrequencyData);
-        }
-
         document.getElementById('start-button').addEventListener('click', () => {
           if (!isListening) {
             audioContext.resume().then(() => {
@@ -197,7 +144,6 @@ document.addEventListener('DOMContentLoaded', () => {
             isListening = false;
             cancelAnimationFrame(animationFrameId); // Stop the animation frame
             console.log('AudioContext is now stopped.');
-
             // affichage des éléments enregistrés dans le tableau 
             document.getElementById("result").innerText = affichage(result);
           }
@@ -206,7 +152,6 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch((error) => {
         console.error('Error accessing microphone:', error);
       });
-
       document.getElementById('reset-button').addEventListener('click', () => {
         index = 0;
         indexLettre = 0;
@@ -215,6 +160,54 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById("result").innerText = "";
       });
 });
+
+function updateFrequencyData() {
+  if (!isListening) {
+    return; // Stop updating when not listening
+  }
+  analyser.getByteFrequencyData(frequencyData);
+
+  // mettre toutes les fréquences en-dessous de la fréquence min à 0
+  for(let i=0; i<-1; i++){
+    frequencyData[i] = 0;
+  }
+
+  // Find the index of the maximum value in the frequencyData array
+
+  const maxIndex = frequencyData.indexOf(Math.max(...frequencyData));
+  maxFrequencyElement.textContent = `Max Frequency: ${maxIndex * frequencyResolution}`;
+  maxFrequencyValueElement.textContent = `Max Frequency Value: ${frequencyData[maxIndex]}`;
+
+  if(frequencyData[maxIndex] >= seuil && maxIndex > START_FREQ/frequencyResolution){
+    console.log("Son au-dessus des seuils détecté"); 
+    register[index] = maxIndex; 
+    
+    // Détection du changement de fréquence
+    if(index > 1){
+      if(register[index] > register[index-1] + marge || register[index] < register[index-1] - marge){
+        console.log("Changement de fréquence détecté");
+        // Mesure du temps pendant lequel une fréquence a été émise
+        let temps = performance.now() - chrono;
+        if(temps > seuil_temps){
+          var lettreTemp = frequencyToChar(register[index-2] * frequencyResolution);
+          for(let i=0; i< Math.floor(temps/seuil_temps); i++){
+            result[indexLettre] = lettreTemp;
+            console.log(lettreTemp);
+            indexLettre ++;
+          }
+          console.log("Temps de la lettre : " + temps);
+        }
+        
+        chrono = performance.now();
+      }
+    }else{
+      chrono = performance.now();
+    }
+    index++;
+  }
+  requestAnimationFrame(updateFrequencyData);
+}
+
 /* menu hamburger*/
 function openNav() {
   document.getElementById("sidebarMenu").style.width = "250px";
@@ -226,3 +219,21 @@ function closeNav() {
   document.body.classList.remove('menu-open');
 }
 
+function syncClockWithNTP() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', "https://worldtimeapi.org/api/ip", true);
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      var responseData = JSON.parse(xhr.responseText);
+      serverTime = new Date(responseData.datetime);
+      ecartAbsoluLocal = serverTime - Date.now(); 
+      console.log(serverTime);
+    } else {
+      console.error('Failed to fetch NTP time');
+    }
+  };
+  xhr.onerror = function() {
+    console.error('Failed to fetch NTP time');
+  };
+  xhr.send();
+}
